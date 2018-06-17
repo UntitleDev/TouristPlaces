4526a8b5cd4fa0d29764c7ba665f22f4ede5ff30
Alix Huerta <alixhuerta16c@cenidet.edu.mx>
2018-06-16 19:57:24 -0500

    Estructura de la app

--- app/src/main/java/mx/com/touristplaces/touristplacesapp/activities/CamaraActivity.java
+++ app/src/main/java/mx/com/touristplaces/touristplacesapp/activities/CamaraActivity.java
@@ -2,16 +2,14 @@ package mx.com.touristplaces.touristplacesapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 import mx.com.touristplaces.touristplacesapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class CamaraActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.i("","");
+        setContentView(R.layout.activity_camara);
     }
 }
